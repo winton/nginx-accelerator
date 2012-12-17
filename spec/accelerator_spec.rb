c2620c2a3ca8a8f94bd994069f5f8e160323d030
Winton Welsh <mail@wintoni.us>
2012-12-17 00:00:36 -0800

    Better specs

--- spec/accelerator_spec.rb
+++ spec/accelerator_spec.rb
@@ -8,18 +8,58 @@ describe Accelerator do
   end
 
   it "should create cache on first request" do
-    `curl http://localhost:8080/test`
     time = Time.new.to_i
+    request.should == time
     body, options = @accelerator.get("/test")
-    body.strip.should == time.to_s
-    options.should == {
-      :ttl => 5,
-      :status => 200,
-      :header => { 
-        :"Content-Length" => 11,
-        :"Content-Type" => "text/plain"
-      },
-      :time => time
-    }
+    response_tests(time)
+  end
+
+  it "should return same cache on next request" do
+    $expires_at = Time.now.to_f.ceil
+    request.should == $last_time
+    body, options = @accelerator.get("/test")
+    response_tests($last_time)
+  end
+
+  it "should not expire naturally before 1 second" do
+    if Time.now.to_f < $expires_at
+      expires_in = $expires_at - Time.now.to_f
+      request(0.5 * expires_in).should == $last_time
+    end
+  end
+
+  it "should expire naturally after 1 second" do
+    if Time.now.to_f < $expires_at
+      expires_in = $expires_at - Time.now.to_f
+      request(expires_in).should == $last_time
+    end
+    response_tests(Time.new.to_i)
+  end
+
+  it "should obey client expiration" do
+    @accelerator.expire("/test")
+    request.should == $last_time
+    response_tests($last_time)
+  end
+
+  it "should set cache body from the client" do
+    @accelerator.set("/test", "123")
+    $expires_at = Time.now.to_f.ceil
+    request.should == 123
+  end
+
+  it "should not expire naturally before 1 second" do
+    if Time.now.to_f < $expires_at
+      expires_in = $expires_at - Time.now.to_f
+      request(0.5 * expires_in).should == 123
+    end
+  end
+
+  it "should expire naturally after 1 second" do
+    if Time.now.to_f < $expires_at
+      expires_in = $expires_at - Time.now.to_f
+      request(expires_in).should == 123
+    end
+    response_tests(Time.new.to_i)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,23 @@ Bundler.require(:development)
 $root = File.expand_path('../../', __FILE__)
 
 require "#{$root}/lib/accelerator"
+
+def request(wait=nil)
+  sleep(wait) if wait
+  `curl -s http://localhost:8080/test`.strip.to_i
+end
+
+def response_tests(time)
+  $last_time = time
+  body, options = @accelerator.get("/test")
+  body.strip.should == time.to_s
+  options.should == {
+    :ttl => 1,
+    :status => 200,
+    :header => { 
+      :"Content-Length" => 11,
+      :"Content-Type" => "text/plain"
+    },
+    :time => time
+  }
+end
